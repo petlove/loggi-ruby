0fddf8731c999784f57f09247fdb1abe1d04b93a
linqueta <lincolnrodrs@gmail.com>
2019-06-24 15:01:30 -0300

    add package status

---
+++ spec/factories/package_status.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+FactoryBot.define do
+  factory :package_status, class: Loggi::PackageStatus do
+    initialize_with { new({}) }
+
+    status { 'ns' }
+    status_display { 'Não iniciada' }
+    detailed_status_display { 'Agendado' }
+    status_code { 1 }
+    updated { Time.parse('2019-04-24 13:48:33.126401') }
+  end
+end
